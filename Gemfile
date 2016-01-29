b358a29d1bcb41642ef08ab3c65fe0acfac53607
David Meyer <david.meyer@appfolio.com>
2016-01-29 14:36:32 -0800

    REAL Initial commit. ActiveRecord and MySQL play nice with Sinatra.

--- Gemfile
+++ Gemfile
@@ -2,5 +2,9 @@ source 'https://rubygems.org'
 gem 'sinatra', :github => "sinatra/sinatra"
 
 # other dependencies
-gem 'http'
+gem 'activerecord'
+gem 'sinatra-activerecord'
+gem 'mysql2'
+gem 'rake'
 gem 'haml'
+gem 'pry'
--- app.rb
+++ app.rb
@@ -1,50 +1,13 @@
 require 'sinatra'
 require 'haml'
+require 'active_record'
+require 'sinatra/activerecord'
+require './models/models.rb'
 
 class HTTPApp < Sinatra::Base
-  set sessions: true
-
-  register do
-    def auth(type)
-      condition do
-        redirect '/login' unless send("is_#{type}?")
-      end
-    end
-  end
-
-  helpers do
-    def is_user?
-      @user != nil
-    end
-    # def protected!
-    #   return if authorized?
-    #   headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
-    #   halt 401, "Not authorized\n"
-    # end
-    #
-    # def authorized?
-    #   @auth ||= Rack::Auth::Basic::Request.new(request.env)
-    #   @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
-    # end
-  end
-
-  before do
-    @user = User.get(session[:user_id])
-  end
+  register Sinatra::ActiveRecordExtension
 
   get '/' do
-    "Hello anonymous"
-  end
-
-  get '/protected', auth: :user do
-    "Hello, #{user.name}."
-  end
-
-  post '/login' do
-    session[:user_id] = User.authenticate(params).id
-  end
-
-  get '/logout' do
-    session[:user_id] = nil
+    haml :index
   end
 end
