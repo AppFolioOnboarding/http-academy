6c7357124765c29b616f6950b853609feb9a3b80
David Meyer <david.meyer@appfolio.com>
2016-01-29 18:52:51 -0800

    Begin experimenting with the User table and authentication

--- app.rb
+++ app.rb
@@ -7,6 +7,26 @@ require './models/models.rb'
 class HTTPApp < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
+  set sessions: true
+
+  register do
+    def auth(type)
+      condition do
+        redirect '/login' unless send("is_#{type}?")
+      end
+    end
+  end
+
+  helpers do
+    def is_user?
+      @user != nil
+    end
+  end
+
+  before do
+    @user = User.find(session[:user_id]) unless User.count == 0
+  end
+
   get '/' do
     haml :index
   end
