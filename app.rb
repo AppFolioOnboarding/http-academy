346efab50c6f042546c4f690ae01a7db7ab4b1c5
David Meyer <david.meyer@appfolio.com>
2016-02-10 15:49:20 -0800

    Add all the FIXME's so that we have guidance

--- app.rb
+++ app.rb
@@ -14,8 +14,19 @@ class HTTPApp < Sinatra::Base
     haml :root
   end
 
+  #############################################################################
   # FIXME: I'm not RESTful!
+  # Idea: In RESTful architecture, we treat everything like a resource. For
+  # example, to visit the page for a specific squirrel, you might navigate to:
+  #
+  # localhost:9292/squirrels/3
+  #
+  # But there are other, less efficient ways to navigate to the page for a
+  # specific squirrel. This "squirrels" route is a great example of how to
+  # violate resource-oriented architecture. How can we fix this?
+  #
   get '/squirrels' do
+    # THIS CODE IS BAD, AND IT SHOULD FEEL BAD. CHANGE IT.
     if params['name'] == 'Leo'
       erb :leo
     elsif params['name'] == 'DDM'
@@ -24,6 +35,12 @@ class HTTPApp < Sinatra::Base
       erb :squirrels
     end
   end
+
+  get '/squirrel' do
+    # CODE GOES HERE
+  end
+
+  #############################################################################
 
   post '/squirrels' do
     name = params[:name]
